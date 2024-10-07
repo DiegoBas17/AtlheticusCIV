742831db3057fa8bc25e2c0e70b5b4d1214760c7
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-10-07 17:28:12 +0200

    add tracker controller

---
+++ src/main/java/diego/basili/AtlheticusCIV/controllers/TrackesController.java
@@ -0,0 +1,76 @@
+package diego.basili.AtlheticusCIV.controllers;
+
+import diego.basili.AtlheticusCIV.entities.Partita;
+import diego.basili.AtlheticusCIV.entities.Tracker;
+import diego.basili.AtlheticusCIV.exceptions.BadRequestException;
+import diego.basili.AtlheticusCIV.payloads.NewEntityRespDTO;
+import diego.basili.AtlheticusCIV.payloads.PartitaDTO;
+import diego.basili.AtlheticusCIV.payloads.TrackerDTO;
+import diego.basili.AtlheticusCIV.services.TrakersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/trackers")
+public class TrackesController {
+    @Autowired
+    private TrakersService trakersService;
+
+    @GetMapping
+    @PreAuthorize("hasAnyAuthority('VISITATORE', 'ATLETA','ADMIN', 'SUPERADMIN')")
+    public Page<Tracker> findAll(@RequestParam(defaultValue = "0") int page,
+                                 @RequestParam(defaultValue = "10") int size,
+                                 @RequestParam(defaultValue = "id") String sortBy) {
+        return this.trakersService.findAll(page, size, sortBy);
+    }
+
+    @PostMapping("/{statisticaId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasAnyAuthority('VISITATORE', 'ATLETA','ADMIN', 'SUPERADMIN')")
+    public NewEntityRespDTO save(@PathVariable UUID statisticaId, @RequestBody @Validated TrackerDTO body, BindingResult validationResult){
+        if (validationResult.hasErrors()) {
+            String messages = validationResult.getAllErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.joining(". "));
+            throw new BadRequestException("Ci sono stati errori nel payload. " + messages);
+        } else {
+            return new NewEntityRespDTO(this.trakersService.saveTracker(body, statisticaId).getId());
+        }
+    }
+
+    @GetMapping("/{trackerId}")
+    @PreAuthorize("hasAnyAuthority('VISITATORE', 'ATLETA','ADMIN', 'SUPERADMIN')")
+    public Tracker findById(@PathVariable UUID trackerId){
+        return this.trakersService.findById(trackerId);
+    }
+
+    @PutMapping("/{trackerId}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public Tracker findByIdAndUpdate(@PathVariable UUID trackerId, @RequestBody @Validated TrackerDTO body, BindingResult validationResult){
+        if (validationResult.hasErrors()) {
+            String messages = validationResult.getAllErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.joining(". "));
+            throw new BadRequestException("Ci sono stati errori nel payload. " + messages);
+        } else {
+            return this.trakersService.findByIdAndUpdate(trackerId, body);
+        }
+    }
+
+    @DeleteMapping("/{trackerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public void findByIdAndDelete(@PathVariable UUID trackerId){
+        this.trakersService.findByIdAndDelete(trackerId);
+    }
+}
