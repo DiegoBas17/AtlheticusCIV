b343720cd35b5fd8ce1a00cdd1cb3a01b1906f61
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-10-07 20:24:15 +0200

    add votiController

---
+++ src/main/java/diego/basili/AtlheticusCIV/controllers/VotiController.java
@@ -0,0 +1,81 @@
+package diego.basili.AtlheticusCIV.controllers;
+
+import diego.basili.AtlheticusCIV.entities.Atleta;
+import diego.basili.AtlheticusCIV.entities.Tracker;
+import diego.basili.AtlheticusCIV.entities.Voto;
+import diego.basili.AtlheticusCIV.exceptions.BadRequestException;
+import diego.basili.AtlheticusCIV.exceptions.NotFoundException;
+import diego.basili.AtlheticusCIV.payloads.NewEntityRespDTO;
+import diego.basili.AtlheticusCIV.payloads.TrackerDTO;
+import diego.basili.AtlheticusCIV.payloads.VotoDTO;
+import diego.basili.AtlheticusCIV.services.VotiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/voti")
+public class VotiController {
+    @Autowired
+    private VotiService votiService;
+
+    @GetMapping
+    @PreAuthorize("hasAnyAuthority('VISITATORE', 'ATLETA','ADMIN', 'SUPERADMIN')")
+    public Page<Voto> findAll(@RequestParam(defaultValue = "0") int page,
+                              @RequestParam(defaultValue = "10") int size,
+                              @RequestParam(defaultValue = "id") String sortBy) {
+        return this.votiService.findAll(page, size, sortBy);
+    }
+
+    @PostMapping("/{statisticaId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPERADMIN')")
+    public NewEntityRespDTO save(@AuthenticationPrincipal Atleta atleta, @PathVariable UUID statisticaId, @RequestBody @Validated VotoDTO body, BindingResult validationResult){
+        if (validationResult.hasErrors()) {
+            String messages = validationResult.getAllErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.joining(". "));
+            throw new BadRequestException("Ci sono stati errori nel payload. " + messages);
+        } else {
+            return new NewEntityRespDTO(this.votiService.saveVoto(atleta, body, statisticaId).getId());
+        }
+    }
+
+    @GetMapping("/{trackerId}")
+    @PreAuthorize("hasAnyAuthority('VISITATORE', 'ATLETA','ADMIN', 'SUPERADMIN')")
+    public Voto findById(@PathVariable UUID trackerId){
+        return this.votiService.findById(trackerId);
+    }
+
+    @PutMapping("/{votoId}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public Voto findByIdAndUpdate(@PathVariable UUID votoId, @RequestBody @Validated VotoDTO body, BindingResult validationResult){
+        if (validationResult.hasErrors()) {
+            String messages = validationResult.getAllErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.joining(". "));
+            throw new BadRequestException("Ci sono stati errori nel payload. " + messages);
+        } else {
+            return this.votiService.findByIdAndUpdate(votoId, body);
+        }
+    }
+
+    @DeleteMapping("/{votoId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public void findByIdAndDelete(@PathVariable UUID votoId){
+        this.votiService.findByIdAndDelete(votoId);
+    }
+}
