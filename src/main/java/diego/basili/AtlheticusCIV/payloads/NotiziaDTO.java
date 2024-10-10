601ae1d4bc814920fd9ee3eea0a73fce7f41ef73
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-10-10 16:29:12 +0200

    add notizie dto

---
+++ src/main/java/diego/basili/AtlheticusCIV/payloads/NotiziaDTO.java
@@ -0,0 +1,17 @@
+package diego.basili.AtlheticusCIV.payloads;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import org.springframework.web.multipart.MultipartFile;
+
+public record NotiziaDTO(@NotEmpty(message = "Il titolo è obbligatoria")
+                         @Size(min = 3, max = 200)
+                         String titolo,
+                         @NotEmpty(message = "Il testo è obbligatoria")
+                         @Size(min = 3, max = 200)
+                         String testo,
+                         MultipartFile immagine,
+                         @NotEmpty(message = "Il autore è obbligatoria")
+                         @Size(min = 3, max = 200)
+                         String autore) {
+}
