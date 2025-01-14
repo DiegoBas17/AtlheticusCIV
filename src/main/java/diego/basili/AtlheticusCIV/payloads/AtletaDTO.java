aadbadc2e6392acf4e7d8195efa926da01827c7b
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2025-01-14 23:13:03 +0100

    edit dto atleta

--- src/main/java/diego/basili/AtlheticusCIV/payloads/AtletaDTO.java
+++ src/main/java/diego/basili/AtlheticusCIV/payloads/AtletaDTO.java
@@ -5,18 +5,18 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
 public record AtletaDTO(@NotEmpty(message = "Il nome è obbligatorio!")
-                        @Size(min = 3, max = 20)
+                        @Size(min = 3)
                         String nome,
                         @NotEmpty(message = "Il cognome è obbligatorio!")
-                        @Size(min = 3, max = 20)
+                        @Size(min = 3)
                         String cognome,
                         @NotEmpty(message = "L'email è obbligatoria")
                         @Email(message = "Email non valida!")
                         String email,
-                        @NotEmpty(message = "Il numeroDiCellulare è obbligatorio!")
-                        @Size(min = 10, max = 10)
+                        @NotEmpty(message = "Il numero di cellulare è obbligatorio!")
+                        @Size(min = 9)
                         String numeroDiCellulare,
                         @NotEmpty(message = "La password è obbligatoria!")
-                        @Size(min = 3, max = 40)
+                        @Size(min = 3)
                         String password) {
 }
